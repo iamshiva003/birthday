79302fa37e9a2f22838ba8eec8731311897bb190
Shivakumar Patil <86395727+iamshiva003@users.noreply.github.com>
2023-05-03 21:17:04 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ with load_page.container():
     with open("index.html", encoding="utf8") as source:
         index_page = source.read()
     
-    html(index_page, height=350)
+    html(index_page, height=400)
 
 placeholder = st.empty()
 col1, col2 = st.columns(2)
@@ -53,4 +53,4 @@ if clicked:
     with open("wishes.html", encoding="utf8") as wish:
         wish_page = wish.read()
             
-    html(wish_page, height=1250)
+    html(wish_page, height=1350)
